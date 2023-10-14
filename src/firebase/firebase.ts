3b4edbab14d018c3bdbf89472ee333697def50fe
seong-94 <togussla@naver.com>
2023-10-15 00:38:17 +0900

    feat: 파이어베이스 설치

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtbSx2kBGkGdTdSvRyAlQu5HmcI7rkd7U",
+  authDomain: "xclone-c9a37.firebaseapp.com",
+  projectId: "xclone-c9a37",
+  storageBucket: "xclone-c9a37.appspot.com",
+  messagingSenderId: "217539480782",
+  appId: "1:217539480782:web:c8051e357f887cf5d6149f",
+  measurementId: "G-QD5VBGZ2Z0",
+};
+
+const app = initializeApp(firebaseConfig);
