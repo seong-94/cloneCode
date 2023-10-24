c6dd7b9191ceed9086d215ea983b5b518698f5f5
seong-94 <togussla@naver.com>
2023-10-24 23:22:00 +0900

    feat: 파이어베이스로 데이터 전송 기능 추가

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAzqjdH5pFTYTHADu8KTY6MHV2HMEjNYrc",
@@ -14,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
